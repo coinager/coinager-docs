98686c22278f26fd829757af2c3e8fd682c92163
Chinmay Purav <chinpurav@gmail.com>
2025-05-29 10:19:40 +0530

    Update deployment branch to 'gh-pages' in Docusaurus configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'coinager', // Usually your GitHub org/user name.
   projectName: 'coinager', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages ',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
