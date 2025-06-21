46bbf3eb61550480bce41f38804ccc8d2c702630
Chinmay Purav <chinpurav@gmail.com>
2025-06-21 21:35:50 +0530

    Update social card image to use favicon.ico for branding consistency

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -54,7 +54,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/logo.svg',
+    image: 'img/favicon.ico',
     navbar: {
       title: 'Coinager',
       logo: {
